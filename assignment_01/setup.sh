92f98161348e4721735dd4d2419014322ec4ec5c
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2025-03-19 22:28:40 +0100

    Update setup script
    Update setup.sh with options for the build environment and build directory handling

--- assignment_01/setup.sh
+++ assignment_01/setup.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-# needed on MacOS
-export LLVM_DIR=/opt/homebrew/opt/llvm
+# Default values and script argument parsing
+opt_macos=false
+opt_linux=false
 
+while getopts ml opt; do
+    case $opt in
+        m) opt_macos=true ;;
+        l) opt_linux=true ;;
+        *) echo 'error in command line parsing' >&2
+           exit 1
+    esac
+done
+
+shift "$(( OPTIND - 1 ))"
+
+# needed on MacOS; exported only with -m flag
+"$opt_macos" && export LLVM_DIR=/opt/homebrew/opt/llvm && echo "Exported MacOS LLVM directory"
+# same for Linux
+"$opt_linux" && export LLVM_DIR=/usr/bin && echo "Exported Linux LLVM directory"
+
+# Actual setup
+
+if [ ! -d build ]; then
+  mkdir build && echo "Created build directory, changing into it..."
+else
+  echo "Build directory already created, changing into it..."
+fi
 cd build
 
 cmake -DLT_LLVM_INSTALL_DIR=$LLVM_DIR ../
