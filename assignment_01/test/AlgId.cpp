25e962139ded86bc0f35a2292b1e0c0ecb621749
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2025-03-26 12:17:45 +0100

    add C++ test functions as-01

---
+++ assignment_01/test/AlgId.cpp
@@ -0,0 +1,9 @@
+int algIdentity(int b) {
+    int a = b + 0;
+    int c = a - 1;
+    int d = 0 + c;
+
+    int e = d * 1;
+    int f = e / 1;
+    return f;
+}
---
+++ assignment_01/test/MultiInst.cpp
@@ -0,0 +1,5 @@
+int multiInstrBase(int b) {
+    int a = b + 1;
+    int c = a - 1;
+    return c;
+}
