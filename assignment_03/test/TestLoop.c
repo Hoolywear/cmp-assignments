6afb1e4789ecffeef8ea3bdc503ff5b8ac472502
Daniele Fassetta <317142@studenti.unimore.it>
2025-04-29 20:10:38 +0200

    add a test for loop invariant instructions

---
+++ assignment_03/test/TestLoop.c
@@ -0,0 +1,16 @@
+int foo() {
+
+    int a = 5;
+    int b = 0;
+    while ( a == 1 ) {
+        int c = a + b;
+    }
+
+    int caca = 200;
+    int pupu = caca + 1;
+
+    if ( pupu > 2 )
+        return caca;
+
+    return pupu;
+}
