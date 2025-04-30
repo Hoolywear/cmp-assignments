9a295a36a7915dbaab55279214d0fd2447d921d2
Daniele Fassetta <317142@studenti.unimore.it>
2025-04-30 21:13:21 +0200

    update as-03 pass

--- assignment_03/As03Pass.cpp
+++ assignment_03/As03Pass.cpp
@@ -41,18 +41,21 @@ bool isLoopInvInstr(Instruction &I, vector<Instruction*> &linvInstr, Loop &L);
 // Function that checks wether an operand from a BinaryOp is considered loop-invariant
 bool isLoopInvOp(Value *OP, vector<Instruction*> &linvInstr, Loop &L) {
   D("\t\tEntered isLoopInvOp for " << *OP);
+
+  if ( isa<ConstantInt>(OP) ) {
+    D("\t\tOperand is constant -> labeling as linv");
+    return true;
+  }
+
   // Cast from Value to Instruction to perform checks
   if (dyn_cast<Instruction>(OP)) {
     Instruction *OpInst = dyn_cast<Instruction>(OP);
 
-    if (find(linvInstr.begin(), linvInstr.end(), OpInst) != linvInstr.end()) {
+    if ( find(linvInstr.begin(), linvInstr.end(), OpInst) != linvInstr.end() ) {
       D("\t\tOperand already labeled as loop-invariant");
       return true;
     } else if (!L.contains(OpInst)) {
       D("\t\tOperand defined outside the loop -> labeling as linv");
-      return true;
-    } else if (isa<ConstantInt>(OpInst)) {
-      D("\t\tOperand is constant -> labeling as linv");
       return true;
     } else if (!OpInst->isBinaryOp()) { // If inside loop, not already linv, not constant, and not BinaryOp, then is not linv
       D("\t\tOperand defined inside the loop and not a BinaryOp -> not linv");
