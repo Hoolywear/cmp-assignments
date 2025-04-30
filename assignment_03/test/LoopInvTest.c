3bae6605330605ace1346631aa9daa5355cf9264
Daniele Fassetta <317142@studenti.unimore.it>
2025-04-30 10:10:43 +0200

    new test for loop invariant instructions

---
+++ assignment_03/test/LoopInvTest.c
@@ -0,0 +1,18 @@
+
+
+void foo(){
+
+    int a = 10;
+    int b = 20;
+
+    for ( int i = 0; i<5; i++ ){
+        if ( i == 3 ){
+            int c = b + a;
+            int f = a + 2;
+        }
+        else {
+            int e = 3;
+        }
+        int d = a + 1;
+    }
+}
