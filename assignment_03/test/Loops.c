480d01e8fdaf93f4690f597ce361df01b829fbb3
Daniele Fassetta <317142@studenti.unimore.it>
2025-04-30 14:43:39 +0200

    update loop invariant test

--- assignment_03/test/Loops.c
+++ assignment_03/test/Loops.c
@@ -1,7 +1,17 @@
-int loop() {
+
+
+int loop(int a, int b, int c, int e) {
+
+
+
     for(int i=0; i<10; i++){
+
         for(int j=0; j<10; j++){
-            print("ciao\n");
+            
+            e = a + b;
+
         }
+
     }
+
 }
