f0364a65f2ed78fa050de13013a3095544d928d4
Daniele Fassetta <317142@studenti.unimore.it>
2025-05-04 21:37:54 +0200

    update tests

--- assignment_03/test/LoopInvTest-1.c
+++ assignment_03/test/LoopInvTest-1.c
@@ -2,7 +2,7 @@
 
 int foo(int a, int b, int c, int e, int d, int f, int k, int j) {
 
-    while ( j == k ) {
+    while ( 1 )  {
         a = 10 + j;
         if (e == 3) {
             d = 5 + b;
@@ -10,6 +10,9 @@ int foo(int a, int b, int c, int e, int d, int f, int k, int j) {
             d = j * a;
         }
 
+        if ( j == f )
+            break;
+        
     }
     b = a + d;
     return f;
