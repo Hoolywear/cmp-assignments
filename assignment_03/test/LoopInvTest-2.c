ccd449db4255fe9b3455f296affeeed1ff549ed7
Daniele Fassetta <317142@studenti.unimore.it>
2025-05-08 13:07:58 +0200

    update test for licm pass

--- assignment_03/test/LoopInvTest-2.c
+++ assignment_03/test/LoopInvTest-2.c
@@ -1,15 +1,20 @@
 int foo(int a, int b, int c, int e, int f, int d, int x){
 
     for ( int i = 0; i<5; i++ ){
-        a = b + c;  
+
+        a = b + c;
+        a = b * 3;
+
         if ( i == 3 ){
             e = x + 3;  
         }
         else if ( i == 4 ) {
             c = a + 10;
-            return c;
+            break;
         }
+
         d = a + 2 ;
+
     }
 
     int p = b + 5;
