7cb6788fedb20cd552b33dac1bdb4e743645cac5
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2025-05-14 15:59:40 +0200

    update test as-04

--- assignment_04/test/TestLoop.c
+++ assignment_04/test/TestLoop.c
@@ -1,15 +1,36 @@
-int foo(int a, int b, int x) {
+int foo(int a, int b, int x, int n) {
+
+    // for (int i = 0; i < a; ++i) {
+    //     int f = 3 * a;
+    // }
+
+    // if (a == b) {
+        
+    //     while(x != 100){
+    //         x = b*2;
+    //     }
+    // }
+
+    // x = a *b;
+
+    // while( a == 200){
+    //     int b = b * 10;
+    // }
     
-    while(x != 100){
-        x = b*2;
-    }
 
-    x = a *b;
+    int i = 0;
+  if (n > 0) {
+    do {
+      // Loop body
+      i += 1;
+    } while (i < n);
+  }
 
-    while( a == 200){
-        int b = b * 10;
-    }
-    
-
+  while (x != 1) {
+    n = a * b;
+  }
+  while (x != 1) {
+    n = a * b;
+  }
     return 0;
 }
