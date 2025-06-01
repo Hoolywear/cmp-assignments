481aadc4dd2a7b5a74327bf762c102d8d6b99fa9
d4ni <317142@studenti.unimore.it>
2025-06-01 16:56:45 +0200

    update tests 3

--- assignment_04/test/TestLoop3.c
+++ assignment_04/test/TestLoop3.c
@@ -1,33 +1,31 @@
 int foo(int a, int b, int x, int n) {
+  int k = 0;
+  do {
 
-  for (int i = 0; i < a; i++) {
-    x = b + 333;
-  }
+    if ( a == b ) {
+      a = a * 1919;
+    }
+    else {
+      a = a * 9191;
+    }
 
-  for (int j = 0; j < a; j++) {
-    x = b + 999;
-  }
+    k++;
+    b = k * 8888;
+  } while( k <= 5 );
 
+  int y = 0;
+  while ( y < 5 ) {
+    if ( a == b ) {
+      a = a * 1919;
+    }
+    else {
+      a = a * 9191;
+    }
 
+    y++;
+    b = y * 2222;
+  } 
 
-
-
-
-
-
-
-
-
-  
-  g = 10;
-  for (int k = 0; k < a; k++) {
-    x = g + 777;
-  }
-  g = 33;
-  for (int i = 1; i <= a; i++) {
-    int popo = g + 5757;
-  }
-  
 
   return 0;
 }
